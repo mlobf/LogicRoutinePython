4283a6b7684c4858bd746992cfbfa5df258a50e8
mlobf <marcoslemeborbafilho@gmail.com>
2021-05-11 21:55:06 -0300

    Terceiro Exercicio

---
+++ logicTest/test3.py
@@ -0,0 +1,12 @@
+"""
+ Escreva um Programa que peça dois números e imprima a soma. 
+"""
+
+print("Coloque os seus dois numeros favoritos")
+
+print("Escreva o seu primeiro numero")
+primeiro = input()
+print("Escreva o seu segundo  numero")
+segundo = input()
+
+print("Aqui estao os seus dois numero favoritos, " + primeiro + " " + segundo)
