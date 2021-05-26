b5dbe44b75f7d72dafb5472ab1f0cd1ed83eb2f9
mlobf <you@example.com>
2021-05-26 09:56:32 -0300

    Teste Commit Git

--- logicTest/test3.py
+++ logicTest/test3.py
@@ -3,10 +3,8 @@
 """
 
 print("Coloque os seus dois numeros favoritos")
-
 print("Escreva o seu primeiro numero")
 primeiro = input()
 print("Escreva o seu segundo  numero")
 segundo = input()
-
 print("Aqui estao os seus dois numero favoritos, " + primeiro + " " + segundo)
